bb24041928fc888276af0a4a80903026141a261f
patrickraguin <patrick.raguin@gmail.com>
2023-02-28 19:11:53 +0100

    ex4

--- lib/player.dart
+++ lib/player.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:tp_dart/bot.dart';
 import 'package:tp_dart/dice.dart';
 import 'package:tp_dart/user_input.dart';
@@ -16,11 +18,38 @@ class Player {
   bool get isAlive => health > 0;
 
   void attackBot(Bot bot) {
-    readEnter('Appuyez sur entrée pour lancer les dés');
-    final dicesValue = rollDices(nickname);
-    final int attack = (dicesValue * strength).round();
-    print('$nickname assène un coup sur le bot avec une force de $attack');
-    bot.health -= attack;
+    final int choice = selectFromMenu('''
+    [1] : Force normale (75% de réussite)
+    [2] : Force doublé (50% de réussite)
+    [3] : Force divisé par 2 (100% de réussite)
+    [4] : Passer son tour et récupérer 50 points de vie
+    ''', 4);
+
+    if (choice == 4) {
+      health += 50;
+      print('$nickname récupère 50 points de vie !');
+    } else {
+      if (choice == 1) {
+        _customAttack(bot, 0.75, 1);
+      } else if (choice == 2) {
+        _customAttack(bot, 0.50, 2);
+      } else {
+        _customAttack(bot, 1, 0.5);
+      }
+    }
+  }
+
+  void _customAttack(Bot bot, double chance, double multiplier) {
+    final double random = Random().nextDouble();
+    if (random <= chance) {
+      readEnter('Appuyez sur entrée pour lancer les dés');
+      final dicesValue = rollDices(nickname);
+      final int attack = (dicesValue * strength * multiplier).round();
+      print('$nickname assène un coup sur le bot avec une force de $attack');
+      bot.health -= attack;
+    } else {
+      print('$nickname n\'a pas réussi à touché le bot !');
+    }
   }
 
   void didWin(Bot bot) {
--- lib/user_input.dart
+++ lib/user_input.dart
@@ -13,3 +13,23 @@ void readEnter(String answer) {
   print(answer);
   stdin.readLineSync();
 }
+
+int readInt(String answer) {
+  int? value;
+  do {
+    value = int.tryParse(readString(answer));
+  } while (value == null);
+  return value;
+}
+
+int selectFromMenu(String message, int max) {
+  int userChoice;
+  do {
+    print(message);
+    userChoice = readInt("Quel est votre choix ?");
+    if (userChoice < 1 || userChoice > max) {
+      print("Veuillez choisir une valeur comprise entre 1 et $max");
+    }
+  } while (userChoice < 1 || userChoice > max);
+  return userChoice;
+}
